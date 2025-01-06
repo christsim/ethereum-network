0a31a5a5534494ed35955db9b34257842aeece47
chris <28301107+christsim@users.noreply.github.com>
2025-01-06 13:03:07 +0200

    added folder permissions

--- base-geth/update.sh
+++ base-geth/update.sh
@@ -1,3 +1,9 @@
+mkdir -p /opt/data/base-geth
+chown -R 1000:1000 /opt/data/base-geth
+
+mkdir -p /opt/data/jwtsecret
+chown -R 1000:1000 /opt/data/jwtsecret
+
 docker-compose pull
 docker-compose down
 docker-compose up -d
